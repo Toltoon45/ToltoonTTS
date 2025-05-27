40d6e0dd8ab90b08c7413be026833f51311a2bb2
Toltoon45 <fedor230400@gmail.com>
2025-05-27 11:15:54 +0300

    Полностью переписан весь код.

---
+++ ToltoonTTS2/Scripts/SaveLoadSettings/GetInstalledVoice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Speech.Synthesis;
+
+namespace ToltoonTTS2.Scripts.SaveLoadSettings
+{
+    class GetInstalledVoice : ILoadAvailableVoices
+    {
+        public void GetListOfAvailableVoices(ObservableCollection<string> comboBoxAvailableVoices)
+        {
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                foreach(InstalledVoice voice in synth.GetInstalledVoices())
+                {
+                    comboBoxAvailableVoices.Add(voice.VoiceInfo.Name);
+                }
+            }
+        }
+    }
+}
