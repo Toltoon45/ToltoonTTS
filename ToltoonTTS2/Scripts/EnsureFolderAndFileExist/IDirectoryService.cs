cae9c58e20c546dec1cd216f28985a654a607644
Toltoon45 <39218960+Toltoon45@users.noreply.github.com>
2025-05-28 18:36:56 +0300

    Revert "New version test"

--- ToltoonTTS2/Scripts/EnsureFolderAndFileExist/IDirectoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ToltoonTTS2.Scripts.EnsureFolderAndFileExist
-{
-    public interface IDirectoryService
-    {
-        void CreateFolder(string folderName, string folderPath);
-        void CreateFile(string fileName, string filePath);
-        void EnsureAppStructureExists();
-    }
-}
