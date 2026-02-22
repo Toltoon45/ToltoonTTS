d46ccb05d9f8cb645d61e57a43339525d4a0db43
toltoon45 <fedor230400@gmail.com>
2026-02-23 00:08:58 +0300

    Текст при необходимости переделывается в русский или в английский транслит, теперь поддерживаются почти все piper голоса но тяжело

--- ToltoonTTS2/Services/TTS/PiperSharpTTS.cs
+++ ToltoonTTS2/Services/TTS/PiperSharpTTS.cs
@@ -1,8 +1,11 @@
 ﻿using NickBuhro.Translit;
+using Nut;
+using Nut.Models;
 using PiperSharp;
 using PiperSharp.Models;
 using System.Globalization;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ToltoonTTS2.Services.TTS
 {
@@ -37,11 +40,28 @@ namespace ToltoonTTS2.Services.TTS
         {
             try
             {
-                text = CleanForTts(text);
-                if (translitToRussian)
+                text = ConvertNumbers(text);
+                bool NeedToTranslitToRussian = voiceName.StartsWith("ru_RU") || voiceName.StartsWith("uk_UA");
+                if (NeedToTranslitToRussian)
                 {
                     text = Transliteration.LatinToCyrillic(text);
+                    text = Regex.Replace(text, "q", "к", RegexOptions.IgnoreCase);
+                    text = Regex.Replace(text, "w", "в", RegexOptions.IgnoreCase);
+                    text = Regex.Replace(text, "y", "и", RegexOptions.IgnoreCase);
                 }
+                else
+                {
+                    text = Transliteration.CyrillicToLatin(text);
+                    text = text.Replace("`", "");
+                    text = Regex.Replace(text, "x", "h", RegexOptions.IgnoreCase);
+                }
+
+                    //if (voiceName.StartsWith )
+                    text = CleanForTts(text);
+                //if (translitToRussian)
+                //{ //не в рашн ёмаё а в английский у тебя и так всё на русском крендель!
+                //    text = Transliteration.LatinToCyrillic(text);
+                //}
                 var cwd = Directory.GetCurrentDirectory();
                 var model = await VoiceModel.LoadModelByKey(voiceName);
                 // чтобы избежать кэширования одинаковых запросов
@@ -70,7 +90,30 @@ namespace ToltoonTTS2.Services.TTS
             
         }
 
-        static string CleanForTts(string input)
+static string ConvertNumbers(string text)
+    {
+        return Regex.Replace(text, @"\d+", match =>
+        {
+            long number;
+
+            // пытаемся распарсить число
+            if (!long.TryParse(match.Value, out number))
+            {
+                return match.Value;
+            }
+
+            try
+            {
+                return number.ToText("ru");
+            }
+            catch
+            {
+                return match.Value;
+            }
+        });
+    }
+
+    static string CleanForTts(string input)
         {
             if (string.IsNullOrEmpty(input))
                 return string.Empty;
